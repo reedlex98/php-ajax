71cff4a4a0e0afbcd416eb4b257d801d7d6e50ee
_rdx98 <elder_louzada@outlook.com>
2020-01-20 17:43:29 -0300

    update unidade 03

--- unidade_03/exercicio_01.php
+++ unidade_03/exercicio_01.php
@@ -6,6 +6,26 @@
     </head>
 
     <body>
+        <div id="load"></div>
 
+        <script src="jquery.js"></script>
+        <script>
+            $.ajax({
+                type: "get",
+                url: "./nome.php",
+                dataType: "text",
+                // success: function (response) {
+                //     console.log(response)
+                // },
+                // error: function(err){
+                //     console.log(err)
+                // }
+            })
+            .done( data => {console.log(data)})
+            .fail( err => {console.log(err)})
+            .always(() => {console.log("Sempre mostrada")})
+            // $('#load').load('./dados.txt')
+
+        </script>
     </body>
 </html>
